7eb6e9e6ffe500d9777c9d3d7e5782cfc7658bb5
tanujkumai <tanujkumai01@gmail.com>
2024-03-15 19:16:25 +0530

    Initial

---
+++ Guessing_Number_Game/App.js
@@ -0,0 +1,8 @@
+import { StyleSheet } from "react-native";
+import StartGameScreen from "./Screens/StartGameScreens";
+
+export default function App() {
+  return <StartGameScreen />;
+}
+
+const styles = StyleSheet.create({});
---
+++ Guessing_Number_Game/Screens/StartGameScreens.js
@@ -0,0 +1,23 @@
+import { StyleSheet, View, TextInput } from "react-native";
+import PrimaryButton from "../Compnents/PrimaryButton";
+
+function StartGameScreen() {
+  return (
+    <View style={styles.inputContainer}>
+      <TextInput />
+      <PrimaryButton>Reset</PrimaryButton>
+      <PrimaryButton>Confirm</PrimaryButton>
+    </View>
+  );
+}
+
+export default StartGameScreen;
+const styles = StyleSheet.create({
+  inputContainer: {
+    marginHorizontal: 24,
+    marginTop: 100,
+    padding: 16,
+    backgroundColor: "#72063c",
+    borderRadius: 8,
+  },
+});
