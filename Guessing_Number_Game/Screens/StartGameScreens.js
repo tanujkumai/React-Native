0c894381c9770a2d12b7faf588ca37aa06dd1da8
tanujkumai <tanujkumai01@gmail.com>
2024-03-20 08:44:00 +0530

    Change Background Color

--- Guessing_Number_Game/Screens/StartGameScreens.js
+++ Guessing_Number_Game/Screens/StartGameScreens.js
@@ -30,7 +30,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
     marginTop: 100,
     padding: 16,
-    backgroundColor: "#4e0329",
+    backgroundColor: "#3b021f",
     borderRadius: 8,
     elevation: 4,
     shadowColor: "black",
