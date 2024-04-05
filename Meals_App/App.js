72a9c0d4d0206d66932535689672b3c084695b37
tanujkumai <tanujkumai01@gmail.com>
2024-04-05 18:45:46 +0530

    Navigation btw two screens

--- Meals_App/App.js
+++ Meals_App/App.js
@@ -1,13 +1,23 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet } from "react-native";
-
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import CategoriesScreens from "./screens/CategoriesScreens";
+import MealsOverviewScreen from "./screens/MealsOverviewScreen";
+
+const stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <>
-      <StatusBar style="light"/>
-      <CategoriesScreens />
+      <StatusBar style="light" />
+      <NavigationContainer>
+        <stack.Navigator>
+          <stack.Screen name="MealsCategory" component={CategoriesScreens} />
+          <stack.Screen name="MealsOverview" component={MealsOverviewScreen} />
+        </stack.Navigator>
+        
+      </NavigationContainer>
     </>
   );
 }
--- Meals_App/screens/CategoriesScreens.js
+++ Meals_App/screens/CategoriesScreens.js
@@ -2,17 +2,25 @@ import { View, FlatList } from "react-native";
 import { CATEGORIES } from "../data/dummy-data";
 import CategoryGridTile from "../components/CategoryGridTile";
 
-function renderCategoryItem(itemData) {
-  return (
-    <CategoryGridTile title={itemData.item.title} color={itemData.item.color} />
-  );
-}
-function CategoriesScreens() {
+function CategoriesScreens( navigation ) {
+  function renderCategoryItem(itemData) {
+    function pressHandeler() {
+      navigation.navitage("MealsOverview");
+    }
+
+    return (
+      <CategoryGridTile
+        title={itemData.item.title}
+        color={itemData.item.color}
+        onPress={pressHandeler}
+      />
+    );
+  }
   return (
     <FlatList
       data={CATEGORIES}
       keyExtractor={(item) => item.id}
-      renderItem={renderCategoryItem}
+      renderItem={renderCategoryItem}.
       numColumns={2}
     />
   );
