40c0eeb553edc1d5f0646872a250f15cd2c4b447
tanujkumai <tanujkumai01@gmail.com>
2024-05-17 19:09:32 +0530

    change

--- 04Meals_App/screens/MealsOverviewScreen.js
+++ 04Meals_App/screens/MealsOverviewScreen.js
@@ -1,8 +1,6 @@
-import { View, FlatList, StyleSheet } from "react-native";
 import { useLayoutEffect } from "react";
-
-import MealItem from "../components/MealItem";
 import { MEALS, CATEGORIES } from "../data/dummy-data";
+import MealsList from "../components/MealsList/MealsList";
 
 function MealsOverviewScreen({ route, navigation }) {
   const catId = route.params.categoryId;
@@ -20,37 +18,7 @@ function MealsOverviewScreen({ route, navigation }) {
       title: categoryTitle,
     });
   }, [catId, navigator]);
-
-  function renderMealItem(itemData) {
-    const item = itemData.item;
-
-    const mealItemProps = {
-      title: item.title,
-      id: item.id,
-      imageUrl: item.imageUrl,
-      affordability: item.affordability,
-      complexity: item.complexity,
-      duration: item.duration,
-    };
-    return <MealItem {...mealItemProps} />;
-  }
-
-  return (
-    <View style={styles.container}>
-      <FlatList
-        data={displayedMeals}
-        keyExtractor={(item) => item.id}
-        renderItem={renderMealItem}
-      />
-    </View>
-  );
+  return <MealsList item={displayedMeals} />;
 }
 
 export default MealsOverviewScreen;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 16,
-  },
-});
