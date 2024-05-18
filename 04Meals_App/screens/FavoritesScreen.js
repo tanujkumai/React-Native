2d3bd8381051fba70e4316ee7102861336575414
tanujkumai <tanujkumai01@gmail.com>
2024-05-18 18:40:24 +0530

    finshed my contxt API use

--- 04Meals_App/screens/FavoritesScreen.js
+++ 04Meals_App/screens/FavoritesScreen.js
@@ -1,20 +1,37 @@
 import { useContext } from "react";
-import {Text} from "react-native"
+import { Text, View, StyleSheet } from "react-native";
 import MealsList from "../components/MealsList/MealsList";
 import { FavoritesContext } from "../store/context/FavoritesContext";
 import { MEALS } from "../data/dummy-data";
 
 function FavoritesScreen() {
   const favoritesMealCxt = useContext(FavoritesContext);
-  if(!favoritesMealCxt) {
-    return <Text>no found</Text>
-  }
 
   const favoriteMeal = MEALS.filter((meal) =>
     favoritesMealCxt.ids.includes(meal.id)
   );
+  if (favoriteMeal.length === 0) {
+    return (
+      <View style={styles.rootContainer}>
+        <Text style={styles.text}>You have no favorite meals yet.</Text>
+      </View>
+    );
+  }
 
   return <MealsList items={favoriteMeal} />;
 }
 
 export default FavoritesScreen;
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "white",
+  },
+});
