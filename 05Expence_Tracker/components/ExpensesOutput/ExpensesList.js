b656d543d472790d2f76bb687e4a36012e995e86
tanujkumai <tanujkumai01@gmail.com>
2024-05-21 21:51:50 +0530

    adding keyExtractor

--- 05Expence_Tracker/components/ExpensesOutput/ExpensesList.js
+++ 05Expence_Tracker/components/ExpensesOutput/ExpensesList.js
@@ -1,11 +1,17 @@
-import { FlatList, Text} from "react-native";
+import { FlatList, Text } from "react-native";
 
 function renderExpenseItem(itemData) {
   return <Text>{itemData.item.description}</Text>;
 }
 
 function ExpensesList({ expenses }) {
-  return <FlatList data={expenses} renderItem={renderExpenseItem} />;
+  return (
+    <FlatList
+      data={expenses}
+      renderItem={renderExpenseItem}
+      keyExtractor={(item) => item.id}
+    />
+  );
 }
 
 export default ExpensesList;
