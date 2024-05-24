3e9d08a959efe0de85c8664297ba9a733b61561b
tanujkumai <tanujkumai01@gmail.com>
2024-05-24 18:16:08 +0530

    create function for updating , removing and confirming new data(right now its hard coded) in list.

--- 05Expence_Tracker/screens/ManageExpense.js
+++ 05Expence_Tracker/screens/ManageExpense.js
@@ -1,10 +1,13 @@
-import { useLayoutEffect } from "react";
+import { useContext, useLayoutEffect } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import IconButton from "../components/UI/IconButton";
 import { GlobalStyles } from "../constants/styles";
 import Button from "../components/UI/Button";
+import { ExpensesContext } from "../store/expenses-context";
 
 function ManageExpense({ route, navigation }) {
+  const expensesCtx = useContext(ExpensesContext);
+
   const editedExpenseId = route.params?.expenseId;
   const isEditing = !!editedExpenseId;
 
@@ -14,11 +17,31 @@ function ManageExpense({ route, navigation }) {
     });
   }, [navigation, isEditing]);
 
-  function deleteExpenseHandler() {}
+  function deleteExpenseHandler() {
+    expensesCtx.deleteExpense(editedExpenseId);
+    navigation.goBack();
+  }
 
-  function cancleHandler() {}
+  function cancleHandler() {
+    navigation.goBack();
+  }
 
-  function confirmHandler() {}
+  function confirmHandler() {
+    if (isEditing) {
+      expensesCtx.updateExpense(editedExpenseId, {
+        description: "testing !!!!!!",
+        amount: 39.99,
+        date: new Date("2024-05-21"),
+      });
+    } else {
+      expensesCtx.addExpense({
+        description: "testing",
+        amount: 19.99,
+        date: new Date("2024-05-23"),
+      });
+    }
+    navigation.goBack();
+  }
 
   return (
     <View style={styles.container}>
