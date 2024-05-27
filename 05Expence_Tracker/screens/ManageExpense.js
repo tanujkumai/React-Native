754dd1b8a46ae61f42c34a299e3e5808bf142b9e
tanujkumai <tanujkumai01@gmail.com>
2024-05-27 19:33:19 +0530

    small changes

--- 05Expence_Tracker/screens/ManageExpense.js
+++ 05Expence_Tracker/screens/ManageExpense.js
@@ -1,16 +1,18 @@
 import { useContext, useLayoutEffect } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, TextInput, View } from "react-native";
+
+import ExpenseForm from "../components/ManageExpense/ExpenseForm";
+import Button from "../components/UI/Button";
 import IconButton from "../components/UI/IconButton";
 import { GlobalStyles } from "../constants/styles";
-import Button from "../components/UI/Button";
 import { ExpensesContext } from "../store/expenses-context";
-import ExpenseForm from "../components/ManageExpense/ExpenseForm";
 
 function ManageExpense({ route, navigation }) {
   const expensesCtx = useContext(ExpensesContext);
 
   const editedExpenseId = route.params?.expenseId;
   const isEditing = !!editedExpenseId;
+
   const selectedExpense = expensesCtx.expenses.find(
     (expense) => expense.id === editedExpenseId
   );
@@ -26,7 +28,7 @@ function ManageExpense({ route, navigation }) {
     navigation.goBack();
   }
 
-  function cancleHandler() {
+  function cancelHandler() {
     navigation.goBack();
   }
 
@@ -42,17 +44,17 @@ function ManageExpense({ route, navigation }) {
   return (
     <View style={styles.container}>
       <ExpenseForm
-        submitButtonHandler={isEditing ? "Update" : "Add"}
+        submitButtonLabel={isEditing ? "Update" : "Add"}
         onSubmit={confirmHandler}
-        onCancle={cancleHandler}
-        defaultValue={selectedExpense}
+        onCancel={cancelHandler}
+        defaultValues={selectedExpense}
       />
       {isEditing && (
         <View style={styles.deleteContainer}>
           <IconButton
             icon="trash"
-            size={36}
             color={GlobalStyles.colors.error500}
+            size={36}
             onPress={deleteExpenseHandler}
           />
         </View>
@@ -71,7 +73,7 @@ const styles = StyleSheet.create({
   },
   deleteContainer: {
     marginTop: 16,
-    padding: 8,
+    paddingTop: 8,
     borderTopWidth: 2,
     borderTopColor: GlobalStyles.colors.primary200,
     alignItems: "center",
