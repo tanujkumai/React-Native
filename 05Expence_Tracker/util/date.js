f175981a18e3de794323960fcc0dbc1ae5b22fa0
tanujkumai <tanujkumai01@gmail.com>
2024-05-27 19:27:50 +0530

    change the formatte of Date

--- 05Expence_Tracker/util/date.js
+++ 05Expence_Tracker/util/date.js
@@ -1,5 +1,5 @@
-export function getformattedDate(date) {
-  return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
+export function getFormattedDate(date) {
+  return date.toISOString().slice(0, 10);
 }
 
 export function getDateMinusDays(date, days) {
